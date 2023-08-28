bcc1ec2dcdd85e973cf40102b594e439a1a95a0d
an-lee <an.lee.work@gmail.com>
2023-08-28 11:50:21 +0800

    update style

---
+++ app/javascript/controllers/card_controller.js
@@ -0,0 +1,61 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static values = { active: Boolean };
+  static targets = ["links", "summary"];
+
+  activeValueChanged(value) {
+    if (value === undefined) return;
+
+    if (value) {
+      this.active();
+    } else {
+      this.inactive();
+    }
+  }
+
+  toggle() {
+    this.activeValue = true;
+  }
+
+  active() {
+    document
+      .querySelectorAll('[data-card-active-value="true"]')
+      .forEach((element) => {
+        if (element === this.element) return;
+        element.dataset.cardActiveValue = false;
+      });
+
+    this.element.classList.add(
+      "active",
+      "border",
+      "sm:rounded-lg",
+      "shadow-xl",
+      "bg-white"
+    );
+    this.element.classList.remove("cursor-pointer");
+    this.summaryTarget.classList.remove(
+      "sm:line-clamp-3",
+      "hidden",
+      "opacity-70"
+    );
+    if (this.hasLinksTarget) {
+      this.linksTarget.classList.remove("hidden");
+    }
+  }
+
+  inactive() {
+    this.element.classList.remove(
+      "active",
+      "border",
+      "sm:rounded-lg",
+      "shadow-xl",
+      "bg-white"
+    );
+    this.element.classList.add("cursor-pointer");
+    this.summaryTarget.classList.add("sm:line-clamp-3", "hidden", "opacity-70");
+    if (this.hasLinksTarget) {
+      this.linksTarget.classList.add("hidden");
+    }
+  }
+}
