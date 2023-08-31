f876bd6501b3e508e2da47189aa27c6565eeb934
an-lee <an.lee.work@gmail.com>
2023-08-31 09:55:58 +0800

    refactor & caching

--- app/controllers/daily_controller.rb
+++ app/controllers/daily_controller.rb
@@ -9,5 +9,7 @@ class DailyController < ApplicationController
       .limit(10)
 
     @page_title = t('.daily_topics')
+    @page_description = @topics.map(&:title_text).join(' | ')
+    @page_keywords = @topics.map(&:tags)&.flatten&.map(&:name)&.uniq&.join(', ')
   end
 end
--- app/javascript/controllers/time_format_controller.js
+++ app/javascript/controllers/time_format_controller.js
@@ -1,13 +1,21 @@
-import { Controller } from '@hotwired/stimulus';
-import dayjs from 'dayjs';
-import utc from 'dayjs/plugin/utc';
-import timezone from 'dayjs/plugin/timezone';
+import { Controller } from "@hotwired/stimulus";
+import dayjs from "dayjs";
+import "dayjs/locale/zh";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+import relativeTime from "dayjs/plugin/relativeTime";
+
 dayjs.extend(utc);
 dayjs.extend(timezone);
+dayjs.extend(relativeTime);
 
 export default class extends Controller {
   static values = {
     timestamp: String,
+    locale: {
+      type: String,
+      default: "en",
+    },
     format: String,
   };
 
@@ -21,14 +29,11 @@ export default class extends Controller {
     let time;
     const timestamp = dayjs.unix(value);
 
-    if (timestamp.isBefore(dayjs().startOf('year'))) {
-      time = timestamp.format('lll');
-    } else if (timestamp.isBefore(dayjs().startOf('day'))) {
-      time = timestamp.format('MM/DD HH:mm');
-    } else if (timestamp.isAfter(dayjs().startOf('day'))) {
-      time = timestamp.format('MM/DD HH:mm');
-    } else if (timestamp.isAfter(dayjs().startOf('year'))) {
-      time = timestamp.format('lll');
+    if (timestamp.isBefore(dayjs().subtract(7, "day"))) {
+      time = timestamp.format("MM/DD/YYYY");
+    } else {
+      const locale = this.localeValue.split("-")[0];
+      time = timestamp.locale(locale).fromNow();
     }
 
     this.element.innerText = time;
