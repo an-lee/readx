57ec084e63d328d4d3660c6ff620f604b84d8cda
an-lee <an.lee.work@gmail.com>
2023-08-31 13:45:50 +0800

    auto refresh datetim display

--- app/javascript/controllers/time_format_controller.js
+++ app/javascript/controllers/time_format_controller.js
@@ -22,12 +22,16 @@ export default class extends Controller {
   connect() {}
 
   timestampValueChanged(value) {
-    if (!value) {
-      return;
-    }
+    if (!value) return;
+
+    this.refreshTimeDisplay();
+  }
+
+  refreshTimeDisplay() {
+    if (!this.timestampValue) return;
 
     let time;
-    const timestamp = dayjs.unix(value);
+    const timestamp = dayjs.unix(this.timestampValue);
 
     if (timestamp.isBefore(dayjs().subtract(7, "day"))) {
       time = timestamp.format("MM/DD/YYYY");
@@ -35,7 +39,36 @@ export default class extends Controller {
       const locale = this.localeValue.split("-")[0];
       time = timestamp.locale(locale).fromNow();
     }
-
     this.element.innerText = time;
+
+    this.scheduleRefresh();
+  }
+
+  scheduleRefresh() {
+    if (!this.timestampValue) return;
+
+    const timestamp = dayjs.unix(this.timestampValue);
+
+    if (timestamp.isAfter(dayjs().subtract(10, "hour"))) {
+      this.timer = setTimeout(() => {
+        this.refreshTimeDisplay();
+      }, 1000 * 60 * 30);
+    } else if (timestamp.isAfter(dayjs().subtract(1, "hour"))) {
+      this.timer = setTimeout(() => {
+        this.refreshTimeDisplay();
+      }, 1000 * 60 * 5);
+    } else if (timestamp.isAfter(dayjs().subtract(10, "minute"))) {
+      this.timer = setTimeout(() => {
+        this.refreshTimeDisplay();
+      }, 1000 * 60);
+    } else if (timestamp.isAfter(dayjs().subtract(1, "minute"))) {
+      this.timer = setTimeout(() => {
+        this.refreshTimeDisplay();
+      }, 1000 * 10);
+    }
+  }
+
+  disconnect() {
+    if (this.timer) clearTimeout(this.timer);
   }
 }
